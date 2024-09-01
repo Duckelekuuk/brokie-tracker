7c23104cfa3f7fbc8cd037dd4829e4220fdd650f
Duckelekuuk <duckelekuuk@gmail.com>
2024-09-01 15:53:18 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
-import Header from "./components/header.jsx";
+import Header from './components/header.jsx';
+import data from './assets/data.json';
+import ExpenseItem from './components/expense-item.jsx';
+import { createContext, useEffect, useMemo, useState } from 'react';
 
+const ListContext = createContext(null);
 function App() {
+    const [list, setList] = useState({});
 
+
+    console.log(enrichList(data));
+
+    function updateList(list) {
+        setList(enrichList(list));
+    }
+
+    useEffect(() => {
+        setList(enrichList(data))
+    }, []);
+
+    const listMemo = useMemo(() => ({list, setList: updateList}), []);
     return (
-        <div className="w-screen h-screen bg-background font-roboto">
-            <Header/>
-        </div>
+        <ListContext.Provider value={listMemo}>
+            <div className="w-screen h-screen bg-background font-roboto">
+                <Header/>
+                <main className={'w-full bg-red-600 flex flex-col items-center'}>
+                    <ExpenseItem title={"In"} value={data.in} type={'IN'} />
+                    <ExpenseItem title={'Out'} value={data.out} type={'OUT'} />
+                </main>
+            </div>
+        </ListContext.Provider>
     )
 }
+
+function enrichList(data) {
+    return {
+        name: data.name,
+        in: computeItem(data.in),
+        out: computeItem(data.out)
+    }
+}
+
+function computeItem(item) {
+    let totalValue = 0;
+    if (Array.isArray(item)) {
+        totalValue = item.map(theItem => computeItem(theItem).totalValue).reduce((prev, curr) => prev + curr, 0);
+    } else {
+        totalValue = item.value;
+    }
+
+    return {
+        ...item,
+        totalValue: totalValue
+    }
+}
+
 
 export default App
--- src/components/expense-item.jsx
+++ src/components/expense-item.jsx
@@ -1,9 +1,21 @@
-export default function ExpenseItem() {
-    return (
-        <>
-            <div className="">
+export default function ExpenseItem({title, value, type}) {
 
+    // const group = Array.isArray(value);
+    // let subItems = [];
+    //
+    // const sum = 0;
+    // if (group) {
+    //
+    // }
+
+    return (
+        <div className={"w-1/2 h-20 bg-zinc-800 my-2 rounded-md"}>
+            <div className="">
+                {title}
             </div>
-        </>
+            <div>
+                <div>{ type === 'IN' ? '+' : '-'} </div>
+            </div>
+        </div>
     );
 }
